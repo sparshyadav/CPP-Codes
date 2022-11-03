57342a4f37b64d576452d89f4daf57c9e0d6db96
sparshyadav <sparshyadavmrt@gmail.com>
2022-11-03 16:28:58 +0530

    Double Pointers

---
+++ Pointers and Dynamic Memory Allocation/Double_Pointers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i=3;
+    int *ptr=&i;
+    int **ptr2=&ptr;
+
+    cout<<"Value Stored at i: "<<i<<endl;
+    cout<<"Value Stored at ptr: "<<*ptr<<endl;
+    cout<<"Value Stored at ptr2: "<<**ptr2<<endl;
+
+    cout<<"Address of i: "<<&i<<endl;
+    cout<<"Address of ptr: "<<&ptr<<endl;
+    cout<<"Address of ptr2: "<<&ptr2<<endl;
+    
+    return 0;
+}
