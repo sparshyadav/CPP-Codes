05c13953fb73fc17bd1ed12529240f6810decac4
sparshyadav <sparshyadavmrt@gmail.com>
2022-11-04 06:27:23 +0530

    Dynamic Memory Allocation

---
+++ Pointers and Dynamic Memory Allocation/Reference_Variable.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void update(int &n){ //this is known as pass by reference, when reference variable is passed in a function
+    cout<<n++<<endl;
+}
+
+int main(){
+
+    int i=5;
+    int &j=i;
+
+    cout<<i<<endl;
+    i++;
+    cout<<i<<endl;
+    j++;
+    cout<<i<<endl;
+
+    cout<<"Before Value: "<<i<<endl;
+    update(i);
+    cout<<"After Value: "<<i<<endl;
+
+    return 0;
+}
