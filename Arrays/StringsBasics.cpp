830a859f7556f89d6d611a80ac80ed808bf457aa
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-08-06 18:55:58 +0530

    Updated

---
+++ Arrays/StringsBasics.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+    // string str="Sparsh";
+    // string str1("Yadav");
+    // cout<<str<<" "<<str1; // Sparsh Yadav
+
+    // string str;
+    // cin>>str; // It only takes input until a space is detected, thus it will only take Sparsh as input when Sparsh Yadav is given as input
+    // cout<<str<<endl;
+
+    // string str;
+    // getline(cin, str); // Using getline we can take multiple words input as Sparsh Yadav.
+    // cout<<str<<endl;
+
+    // char ch='a';
+    // cout<<int(ch)<<endl; //It displays the ASCII value of the character.
+
+    // string str="Sparsh Yadav";
+    // cout<<str<<endl;
+    // reverse(str.begin(), str.end()); // Reverse function is used to reverse a string and starting pointer and ending pointer is used to reverse the string.
+    // cout<<str<<endl;
+
+    // string str="Sparsh Yadav";
+    // cout<<str.substr(0, 6)<<endl; // Substr is used to print the substring of a string from the starting index to the ending index.
+    // cout<<str.substr(1); // If only one index if provided to substr, then it prints the substring from the given index.
+
+    // string str="Sparsh";
+    // string str2="Yadav";
+    // cout<<str+str2; // + operator in string is used to concat two or more string together.
+
+    
+
+    return 0;
+}
