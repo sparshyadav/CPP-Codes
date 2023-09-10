2221d92767a72223c186cfa54cee1bee5c13e64b
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-09-10 16:16:01 +0530

    Stacks Using Arrays

--- Stack/Stacks_Basics.cpp
+++ Stack/Stacks_Basics.cpp
@@ -1,55 +1,97 @@
-#include<iostream>
+#include <iostream>
+#include<climits>
 using namespace std;
 
-class Stack{
+class Stack
+{
     int capacity;
-    int* arr;
+    int *arr;
     int top;
-    public:
-    Stack(int c){
-        this->capacity=c;
-        arr=new int[c];
-        this->top=-1;
+
+public:
+    Stack(int c)
+    {
+        this->capacity = c;
+        arr = new int[c];
+        this->top = -1;
     }
-    void push(int data){
-        if(this->top== this->capacity-1){
-            cout<<"Stack Overflow"<<endl;
+
+    void push(int data)
+    {
+        if (this->top == this->capacity - 1)
+        {
+            cout<<endl;
+            cout << "Stack Overflow" << endl;
             return;
         }
         this->top++;
-        this->arr[this->top]=data; 
+        this->arr[this->top] = data;
+
     }
-    void pop(){
-        if(this->top==-1){
-            cout<<"Stack Underflow"<<endl;
+
+    void pop()
+    {
+        if (this->top == -1)
+        {
+            cout << "Stack Underflow" << endl;
             return;
         }
         this->top--;
     }
-    int top(){
-        if(this->top==capacity-1){
-            cout<<"Stack Overflow"<<endl;
-            return;
+
+    int getTop()
+    {
+        if (this->top == capacity - 1)
+        {
+            cout << "Stack Overflow" << endl;
+            return INT_MIN;
         }
+        cout<<endl;
         return this->arr[this->top];
     }
-    bool isEmpty(){
-        if(top==-1){
+
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
             return 1;
         }
         return 0;
     }
-    bool isFull(){
-        return this->top==capacity-1;
+
+    bool isFull()
+    {
+        return this->top == capacity - 1;
     }
-    int size(){
-        return this->top+1;
+
+    int size()
+    {
+        cout<<endl;
+        return this->top + 1;
     }
+
 };
 
-int main(){
+int main()
+{
 
+    Stack st(5);
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
 
+    cout << st.getTop();
+    st.push(5);
+    cout << st.getTop();
+    // st.pop();
+    st.pop();
+    st.pop();
+    st.push(10);    
+    // st.push(15); 
+    int size=st.size();
+    cout<<size;  
+    cout<<st.getTop();
 
     return 0;
 }
