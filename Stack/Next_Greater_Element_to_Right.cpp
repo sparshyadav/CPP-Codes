96ee32478a70a0386c6f085fb8075f77a52267a4
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-09-13 07:32:43 +0530

    Next Greater Element to Right using Stack

---
+++ Stack/Next_Greater_Element_to_Right.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+vector<int> NGR(vector<int> &arr){
+    vector<int> ans(arr.size(), -1);
+    stack<int> st;
+    
+    for(int i=arr.size()-1; i>=0; i--){
+        if(st.size()>0 && st.top()>arr[i]){
+            ans[i]=st.top();
+        }
+        else if(st.size()>0 && st.top()<arr[i]){
+            while(!st.empty() && st.top()<arr[i]){
+            st.pop();
+            }
+            if(!st.empty() && st.top()>arr[i]){
+                ans[i]=st.top();
+            }
+        }
+        st.push(arr[i]);
+    }
+    return ans;
+}
+
+int main(){
+
+    vector<int> arr={1, 3, 0, 1, 2, 4};
+    vector<int> result=NGR(arr);
+
+    for(int i=0; i<result.size(); i++){
+        cout<<result[i]<<" ";
+    }
+
+    return 0;
+}
