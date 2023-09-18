76c7e396306116d272be7a56863703706e3f7c73
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-09-19 01:56:14 +0530

    Arithmetic Calculator Infix Expression

---
+++ Stack/Infix_Expression.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+using namespace std;
+
+int ans(string str)
+{
+    stack<int> num;
+    stack<char> op;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        int ascii = int(str[i]);
+        if (ascii >= 48 && ascii <= 57)
+        {
+            num.push(ascii - 48);
+        }
+        else if (op.size() == 0)
+        {
+            op.push(str[i]);
+        }
+        else
+        {
+            if (str[i] == '+' || str[i] == '-')
+            {
+                int v2 = num.top();
+                num.pop();
+                int v1 = num.top();
+                num.pop();
+                if (op.top() == '-')
+                {
+                    num.push(v1 - v2);
+                }
+                else if (op.top() == '+')
+                {
+                    num.push(v1 + v2);
+                }
+                else if (op.top() == '*')
+                {
+                    num.push(v1 * v2);
+                }
+                else if (op.top() == '/')
+                {
+                    num.push(v1 / v2);
+                }
+                op.pop();
+                op.push(str[i]);
+            }
+            else if (str[i] == '*' || str[i] == '/')
+            {
+                if (op.top() == '*' || op.top() == '/')
+                {
+                    int v2 = num.top();
+                    num.pop();
+                    int v1 = num.top();
+                    num.pop();
+                    if (op.top() == '*')
+                    {
+                        num.push(v1 * v2);
+                    }
+                    else if (op.top() == '/')
+                    {
+                        num.push(v1 / v2);
+                    }
+                    op.pop();
+                    op.push(str[i]);
+                }
+                else
+                {
+                    op.push(str[i]);
+                }
+            }
+        }
+    }
+
+    while (num.size() > 1)
+    {
+        int v2 = num.top();
+        num.pop();
+        int v1 = num.top();
+        num.pop();
+        if (op.top() == '-')
+        {
+            num.push(v1 - v2);
+        }
+        else if (op.top() == '+')
+        {
+            num.push(v1 + v2);
+        }
+        else if (op.top() == '*')
+        {
+            num.push(v1 * v2);
+        }
+        else if (op.top() == '/')
+        {
+            num.push(v1 / v2);
+        }
+        op.pop();
+    }
+    return num.top();
+}
+
+int main()
+{
+
+    string str = "9-5+3*4/6";
+    int result = ans(str);
+    cout << result;
+
+    return 0;
+}
