a075d7494fd52913643b1eb2f84dcc1d29faca71
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-09-20 01:47:09 +0530

    Repo Updated

--- Searching and Sorting/Bubble_Sort.cpp
+++ Searching and Sorting/Bubble_Sort.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 void bubbleSort(int arr[], int size)
 {
-    for(int i=0; i<size; i++){
-        for(int j=0; j<size-i; j++){
-            if(arr[i]>arr[j]){
-                swap(arr[i], arr[j]);
+    int n=size;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
@@ -27,7 +31,7 @@ int main()
     bubbleSort(arr, 10);
     for (int i = 0; i < 10; i++)
     {
-        cout << arr[i];
+        cout << arr[i] << " ";
     }
 
     return 0;
---
+++ Stack/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+op.push(str[i]);
