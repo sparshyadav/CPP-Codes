73547a74e43062146540c6ee77e67f398612142d
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-09-21 13:09:47 +0530

    Isomorphic Strings

---
+++ Hashing/Isomorphic_Strings.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string str="aab";
+    string s="xxy";
+
+    map<char, char> ans;
+    for(int i=0; i<str.length(); i++){
+        if(ans.find(str[i])!=ans.end()){
+            
+        }
+    }
+
+    return 0;
+}
