cf941f68a318b97d316989fcc1b8f93c42c5cf1e
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-12-01 10:16:43 +0530

    Adding Value in HashTable

--- Hashing/Hashing.cpp
+++ Hashing/Hashing.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Hashing{
+    vector<list<int>> hashtable;
+    int buckets;
+
+    Hashing(int size){
+        buckets=size;
+        hashtable.resize(size);
+    }
+
+    int hashvalue(int key){
+        return key%buckets;
+    }
+
+    int add(int key){
+        int idx=hashvalue(key);
+        hashtable[idx].push_back(key);
+    }
+};
+
 int main()
 {
 
