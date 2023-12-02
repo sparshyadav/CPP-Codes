5c893e5a2e9fcf11a4fa806f37b732be6110ce39
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-12-02 16:04:20 +0530

    Creating a Hashmap

--- Hashing/HashMap.cpp
+++ Hashing/HashMap.cpp
@@ -4,8 +4,21 @@ using namespace std;
 
 int main()
 {
+    map<string, int> directory; // By Default creating an ascending hashmap
+    directory["Sparsh"]=5328795;
+    directory["Shreya"]=5436334;
 
-   
+    map<string, int, greater<string>> directory2; // Creating an Descending hashmap
+    directory2["Sparsh"]=5328795;
+    directory2["Shreya"]=5436334;
+
+    for(auto element: directory){ // Iterating through the hashmap
+        cout<<"Name - "<<element.first<<" Number - "<<element.second<<endl;;
+    }
+
+    for(auto element: directory2){
+        cout<<"Name - "<<element.first<<" Number - "<<element.second<<endl;;
+    }
 
     return 0;
 }
