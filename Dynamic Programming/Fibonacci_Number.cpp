86c53a44c03a216464969a68f030be38e903db0c
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-01-17 17:18:38 +0530

    Fibonacci Number Using DP Using Tabulation

--- Dynamic Programming/Fibonacci_Number.cpp
+++ Dynamic Programming/Fibonacci_Number.cpp
@@ -25,7 +25,21 @@ int main()
 
     vector<int> dp(n + 1, -1);
 
-    cout << fibo(n, dp);
+    // Memoisation
+    cout << fibo(n, dp) << endl;
+
+    // Tabulation
+    int prev2 = 0;
+    int prev1 = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    cout << prev1;
 
     return 0;
 }
