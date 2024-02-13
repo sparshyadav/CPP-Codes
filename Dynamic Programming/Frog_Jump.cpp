b9bf7945d73db9b493949c42ce6db9cc0f38d5d6
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-02-13 15:00:40 +0530

    Frog Jump Using Memoization

--- Dynamic Programming/Frog_Jump.cpp
+++ Dynamic Programming/Frog_Jump.cpp
@@ -1,18 +1,52 @@
-// Recursion
+// // Recursion
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int frogJump(int n, int heights[])
+// {
+//     if (n <= 1)
+//     {
+//         return 0;
+//     }
+
+//     int left = frogJump(n - 1, heights) + abs(heights[n] - heights[n - 1]);
+//     int right = frogJump(n - 2, heights) + abs(heights[n] - heights[n - 2]);
+
+//     return min(left, right);
+// }
+
+// int main()
+// {
+
+//     int n = 6;
+//     int heights[6] = {30, 10, 60, 10, 60, 50};
+
+//     int ans = frogJump(5, heights);
+//     cout << ans;
+
+//     return 0;
+// }
+
+// Memoization
 #include <bits/stdc++.h>
 using namespace std;
 
-int frogJump(int n, int heights[])
+int memo(int n, int heights[], vector<int> &dp)
 {
     if (n <= 1)
     {
         return 0;
     }
 
-    int left = frogJump(n - 1, heights) + abs(heights[n] - heights[n - 1]);
-    int right = frogJump(n - 2, heights) + abs(heights[n] - heights[n - 2]);
+    if (dp[n] != -1)
+    {
+        return dp[n];
+    }
 
-    return min(left, right);
+    int left = memo(n - 1, heights, dp) + abs(heights[n] - heights[n - 1]);
+    int right = memo(n - 2, heights, dp) + abs(heights[n] - heights[n - 2]);
+
+    return dp[n] = min(left, right);
 }
 
 int main()
@@ -21,7 +55,9 @@ int main()
     int n = 6;
     int heights[6] = {30, 10, 60, 10, 60, 50};
 
-    int ans = frogJump(5, heights);
+    vector<int> dp(n + 1, -1);
+
+    int ans = memo(5, heights, dp);
     cout << ans;
 
     return 0;
