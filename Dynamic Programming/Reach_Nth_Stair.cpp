04747c45878499cf13344bae44474c3dbb59f8b1
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-02-13 12:47:41 +0530

    Finding the Number of Ways to Reach Nth Stair Using Memoization

--- Dynamic Programming/Reach_Nth_Stair.cpp
+++ Dynamic Programming/Reach_Nth_Stair.cpp
@@ -1,8 +1,40 @@
-// Recursion
+// // Recursion
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int climbStairs(int n)
+// {
+//     if (n == 0)
+//     {
+//         return 1;
+//     }
+
+//     if (n < 0)
+//     {
+//         return 0;
+//     }
+
+//     return climbStairs(n - 1) + climbStairs(n - 2);
+// }
+
+// int main()
+// {
+
+//     int n;
+//     cout << "Enter the Number of Stairs you have to Climb: " << endl;
+//     cin >> n;
+
+//     int ans = climbStairs(n);
+//     cout << ans;
+
+//     return 0;
+// }
+
+// Memoization
 #include <bits/stdc++.h>
 using namespace std;
 
-int climbStairs(int n)
+int memo(int n, vector<int> &dp)
 {
     if (n == 0)
     {
@@ -14,7 +46,12 @@ int climbStairs(int n)
         return 0;
     }
 
-    return climbStairs(n - 1) + climbStairs(n - 2);
+    if (dp[n] != -1)
+    {
+        return dp[n];
+    }
+
+    return dp[n] = memo(n - 1, dp) + memo(n - 2, dp);
 }
 
 int main()
@@ -24,7 +61,9 @@ int main()
     cout << "Enter the Number of Stairs you have to Climb: " << endl;
     cin >> n;
 
-    int ans = climbStairs(n);
+    vector<int> dp(n + 1, -1);
+
+    int ans = memo(n, dp);
     cout << ans;
 
     return 0;
