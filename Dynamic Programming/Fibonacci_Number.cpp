d7221bf350318055f2ad0013c834f0f1782ceded
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-02-13 12:07:27 +0530

    Finding Fibonacci Number Using - Recursion, Memoization, Tabulation, Tabulation with Space Optimization

--- Dynamic Programming/Fibonacci_Number.cpp
+++ Dynamic Programming/Fibonacci_Number.cpp
@@ -25,6 +25,8 @@
 //     return 0;
 // }
 
+
+
 // Memoization
 // #include <bits/stdc++.h>
 // using namespace std;
@@ -58,22 +60,59 @@
 //     return 0;
 // }
 
-// Tabulation
+
+
+// // Tabulation
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int tab(int n)
+// {
+//     vector<int> dp(n + 1);
+//     dp[0] = 0;
+//     dp[1] = 1;
+
+//     for (int i = 2; i <= n; i++)
+//     {
+//         dp[i] = dp[i - 1] + dp[i - 2];
+//     }
+
+//     return dp[n];
+// }
+
+// int main()
+// {
+
+//     int n;
+//     cout << "Enter the Nth Term of Fibonacci Series:" << endl;
+//     cin >> n;
+
+//     int ans = tab(n);
+//     cout << ans;
+
+//     return 0;
+// }
+
+
+
+// Tabulation With Space Optimization -> O(1)
 #include <bits/stdc++.h>
 using namespace std;
 
 int tab(int n)
 {
-    vector<int> dp(n + 1);
-    dp[0] = 0;
-    dp[1] = 1;
+    int curr;
+    int prev = 1;
+    int prev2 = 0;
 
     for (int i = 2; i <= n; i++)
     {
-        dp[i] = dp[i - 1] + dp[i - 2];
+        curr = prev + prev2;
+        prev2 = prev;
+        prev = curr;
     }
 
-    return dp[n];
+    return prev;
 }
 
 int main()
