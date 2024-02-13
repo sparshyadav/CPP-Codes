987f926d137d75a10c3b92830cf5978c50b19e3a
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-02-13 12:54:54 +0530

    Finding the Number of Ways to Reach Nth Stair Using Tablulation

--- Dynamic Programming/Reach_Nth_Stair.cpp
+++ Dynamic Programming/Reach_Nth_Stair.cpp
@@ -31,27 +31,60 @@
 // }
 
 // Memoization
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int memo(int n, vector<int> &dp)
+// {
+//     if (n == 0)
+//     {
+//         return 1;
+//     }
+
+//     if (n < 0)
+//     {
+//         return 0;
+//     }
+
+//     if (dp[n] != -1)
+//     {
+//         return dp[n];
+//     }
+
+//     return dp[n] = memo(n - 1, dp) + memo(n - 2, dp);
+// }
+
+// int main()
+// {
+
+//     int n;
+//     cout << "Enter the Number of Stairs you have to Climb: " << endl;
+//     cin >> n;
+
+//     vector<int> dp(n + 1, -1);
+
+//     int ans = memo(n, dp);
+//     cout << ans;
+
+//     return 0;
+// }
+
+// Tabulation
 #include <bits/stdc++.h>
 using namespace std;
 
-int memo(int n, vector<int> &dp)
+int tab(int n)
 {
-    if (n == 0)
+    vector<int> dp(n + 1);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
     {
-        return 1;
+        dp[i] = dp[i - 1] + dp[i - 2];
     }
 
-    if (n < 0)
-    {
-        return 0;
-    }
-
-    if (dp[n] != -1)
-    {
-        return dp[n];
-    }
-
-    return dp[n] = memo(n - 1, dp) + memo(n - 2, dp);
+    return dp[n];
 }
 
 int main()
@@ -61,9 +94,7 @@ int main()
     cout << "Enter the Number of Stairs you have to Climb: " << endl;
     cin >> n;
 
-    vector<int> dp(n + 1, -1);
-
-    int ans = memo(n, dp);
+    int ans = tab(n);
     cout << ans;
 
     return 0;
