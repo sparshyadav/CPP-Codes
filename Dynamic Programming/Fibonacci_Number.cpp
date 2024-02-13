68338c03dcc48f08dd03c9a815e71225f00bbb4e
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-02-13 11:54:10 +0530

    Finding Fibonacci Number Using Memoization

--- Dynamic Programming/Fibonacci_Number.cpp
+++ Dynamic Programming/Fibonacci_Number.cpp
@@ -1,15 +1,47 @@
 // Recursion
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int fibo(int n)
+// {
+//     if (n <= 1)
+//     {
+//         return n;
+//     }
+
+//     return fibo(n - 1) + fibo(n - 2);
+// }
+
+// int main()
+// {
+
+//     int n;
+//     cout << "Enter the Nth Term of Fibonacci Series:" << endl;
+//     cin >> n;
+
+//     int ans = fibo(n);
+//     cout << ans;
+
+//     return 0;
+// }
+
+// Memoization
 #include <bits/stdc++.h>
 using namespace std;
 
-int fibo(int n)
+int memo(int n, vector<int> &dp)
 {
     if (n <= 1)
     {
         return n;
     }
 
-    return fibo(n - 1) + fibo(n - 2);
+    if (dp[n] != -1)
+    {
+        return dp[n];
+    }
+
+    return memo(n - 1, dp) + memo(n - 2, dp);
 }
 
 int main()
@@ -19,7 +51,8 @@ int main()
     cout << "Enter the Nth Term of Fibonacci Series:" << endl;
     cin >> n;
 
-    int ans = fibo(n);
+    vector<int> dp(n + 1, -1);
+    int ans = memo(n, dp);
     cout << ans;
 
     return 0;
