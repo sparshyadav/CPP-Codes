355166d218e021c204dcc604bfcb79e43c573885
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-02-13 11:50:20 +0530

    Finding Fibonacci Number Using Recursion

--- Dynamic Programming/Fibonacci_Number.cpp
+++ Dynamic Programming/Fibonacci_Number.cpp
@@ -1,19 +1,15 @@
+// Recursion
 #include <bits/stdc++.h>
 using namespace std;
 
-int fibo(int n, vector<int> &dp)
+int fibo(int n)
 {
-    if (n <= 1) // Base Case
+    if (n <= 1)
     {
         return n;
     }
 
-    if (dp[n] != -1) // Checking if Previously Calculated Recursion Result
-    {
-        return dp[n];
-    }
-
-    return dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp); // Returning and Storing the Result in dp Vector
+    return fibo(n - 1) + fibo(n - 2);
 }
 
 int main()
@@ -23,23 +19,8 @@ int main()
     cout << "Enter the Nth Term of Fibonacci Series:" << endl;
     cin >> n;
 
-    vector<int> dp(n + 1, -1);
-
-    // Memoisation
-    cout << fibo(n, dp) << endl;
-
-    // Tabulation
-    int prev2 = 0;
-    int prev1 = 1;
-
-    for (int i = 2; i <= n; i++)
-    {
-        int curr = prev1 + prev2;
-        prev2 = prev1;
-        prev1 = curr;
-    }
-
-    cout << prev1;
+    int ans = fibo(n);
+    cout << ans;
 
     return 0;
 }
