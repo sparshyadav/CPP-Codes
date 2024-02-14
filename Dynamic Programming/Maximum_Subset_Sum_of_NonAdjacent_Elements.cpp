5cfa6d5a183b700be711d425d4c582bcf8c94ebe
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-02-14 13:32:21 +0530

    Finding the Maximum Subset Sum of Non Adjacent Elements Using Tabulation

--- Dynamic Programming/Maximum_Subset_Sum_of_NonAdjacent_Elements.cpp
+++ Dynamic Programming/Maximum_Subset_Sum_of_NonAdjacent_Elements.cpp
@@ -41,30 +41,82 @@
 // }
 
 // Memoization
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int memo(int arr[], int index, vector<int> &dp)
+// {
+//     if (index == 0)
+//     {
+//         return arr[0];
+//     }
+
+//     if (index < 0)
+//     {
+//         return 0;
+//     }
+
+//     if (dp[index] != -1)
+//     {
+//         return dp[index];
+//     }
+
+//     int pick = arr[index] + memo(arr, index - 2, dp);
+//     int notPick = 0 + memo(arr, index - 1, dp);
+
+//     return dp[index] = max(pick, notPick);
+// }
+
+// int main()
+// {
+
+//     int n;
+//     cout << "Enter the Number of Elements You Want in the Array" << endl;
+//     cin >> n;
+
+//     int arr[n];
+//     cout << "Enter the Elements in Your Array" << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     vector<int> dp(n + 1, -1);
+
+//     int sum = memo(arr, n - 1, dp);
+//     cout << sum;
+
+//     return 0;
+// }
+
+// Tabulation
 #include <bits/stdc++.h>
 using namespace std;
 
-int memo(int arr[], int index, vector<int> &dp)
+int tab(int arr[], int n)
 {
-    if (index == 0)
+    vector<int> dp(n + 1);
+    dp[0] = arr[0];
+    int neg = 0;
+
+    for (int i = 1; i < n; i++)
     {
-        return arr[0];
+        int pick = 0;
+        if (i - 2 >= 0)
+        {
+            pick = arr[i] + dp[i - 2];
+        }
+        else
+        {
+            pick = arr[i] + neg;
+        }
+
+        int notPick = 0 + dp[i - 1];
+
+        dp[i] = max(pick, notPick);
     }
 
-    if (index < 0)
-    {
-        return 0;
-    }
-
-    if (dp[index] != -1)
-    {
-        return dp[index];
-    }
-
-    int pick = arr[index] + memo(arr, index - 2, dp);
-    int notPick = 0 + memo(arr, index - 1, dp);
-
-    return dp[index] = max(pick, notPick);
+    return dp[n - 1];
 }
 
 int main()
@@ -81,9 +133,7 @@ int main()
         cin >> arr[i];
     }
 
-    vector<int> dp(n + 1, -1);
-
-    int sum = memo(arr, n - 1, dp);
+    int sum = tab(arr, n);
     cout << sum;
 
     return 0;
