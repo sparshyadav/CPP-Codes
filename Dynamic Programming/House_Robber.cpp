394064abb3906bbaa8c74e0517d6c73ab2f895dd
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-17 12:14:38 +0530

    Updated File

--- Dynamic Programming/House_Robber.cpp
+++ Dynamic Programming/House_Robber.cpp
@@ -24,31 +24,66 @@
 //     cout<<ans;
 // }
 
-// Memoization
+// // Memoization
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int solve(int n, int arr[], vector<int> &dp)
+// {
+//     if (n == 0)
+//     {
+//         return arr[0];
+//     }
+
+//     if (n < 0)
+//     {
+//         return 0;
+//     }
+
+//     if (dp[n] != -1)
+//     {
+//         return dp[n];
+//     }
+
+//     int pick = arr[n] + solve(n - 2, arr, dp);
+//     int notPick = solve(n - 1, arr, dp);
+
+//     return dp[n] = max(pick, notPick);
+// }
+
+// int main()
+// {
+//     int arr[5] = {2, 7, 9, 3, 1};
+//     vector<int> dp(5, -1);
+
+//     int ans = solve(4, arr, dp);
+
+//     cout << ans;
+// }
+
+// Tabulation
 #include <bits/stdc++.h>
 using namespace std;
 
 int solve(int n, int arr[], vector<int> &dp)
 {
-    if (n == 0)
+    dp[0] = arr[0];
+    int neg = 0;
+
+    for (int i = 1; i < n; i++)
     {
-        return arr[0];
+        int pick = arr[i];
+        if (i > 1)
+        {
+            pick += dp[i - 2];
+        }
+
+        int notPick = arr[i - 1];
+
+        dp[i] = max(pick, notPick);
     }
 
-    if (n < 0)
-    {
-        return 0;
-    }
-
-    if (dp[n] != -1)
-    {
-        return dp[n];
-    }
-
-    int pick = arr[n] + solve(n - 2, arr, dp);
-    int notPick = solve(n - 1, arr, dp);
-
-    return dp[n] = max(pick, notPick);
+    return dp[n];
 }
 
 int main()
@@ -56,7 +91,7 @@ int main()
     int arr[5] = {2, 7, 9, 3, 1};
     vector<int> dp(5, -1);
 
-    int ans = solve(4, arr, dp);
+    int ans = solve(5, arr, dp);
 
     cout << ans;
 }
