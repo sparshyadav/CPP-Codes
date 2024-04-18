36cbe7cfae882d50681e25c60e1f1f6bead502af
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-18 11:50:03 +0530

    Finding All Unique Paths Using Tabulation with Space Optimization

--- Dynamic Programming/Count_All_Unique_Paths.cpp
+++ Dynamic Programming/Count_All_Unique_Paths.cpp
@@ -66,46 +66,95 @@
 // }
 
 // Tabulation
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int solve(int i, int j, vector<vector<int>> &dp)
+// {
+//     for (int row = 0; row < i; row++)
+//     {
+//         for (int col = 0; col < j; col++)
+//         {
+//             if (row == 0 && col == 0)
+//             {
+//                 dp[0][0] = 1;
+//             }
+//             else
+//             {
+//                 int up = 0;
+//                 if (row > 0)
+//                 {
+//                     up = dp[row - 1][col];
+//                 }
+
+//                 int left = 0;
+//                 if (col > 0)
+//                 {
+//                     left = dp[row][col - 1];
+//                 }
+
+//                 dp[row][col] = up + left;
+//             }
+//         }
+//     }
+
+//     return dp[i - 1][j - 1];
+// }
+
+// int main()
+// {
+//     vector<vector<int>> dp(3, vector<int>(7, -1));
+
+//     int ans = solve(3, 7, dp);
+//     cout << ans;
+
+//     return 0;
+// }
+
+// Tabulation with Space Optimization
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(int i, int j, vector<vector<int>> &dp)
+int solve(int i, int j)
 {
+    vector<int> prev(j, 0);
+
     for (int row = 0; row < i; row++)
     {
+        vector<int> curr(j, 0);
         for (int col = 0; col < j; col++)
         {
             if (row == 0 && col == 0)
             {
-                dp[0][0] = 1;
+                curr[0] = 1;
             }
             else
             {
                 int up = 0;
                 if (row > 0)
                 {
-                    up = dp[row - 1][col];
+                    up = prev[col];
                 }
 
                 int left = 0;
                 if (col > 0)
                 {
-                    left = dp[row][col - 1];
+                    left = curr[col - 1];
                 }
 
-                dp[row][col] = up + left;
+                curr[col] = up + left;
             }
         }
+
+        prev = curr;
     }
 
-    return dp[i - 1][j - 1];
+    return prev[j - 1];
 }
 
 int main()
 {
-    vector<vector<int>> dp(3, vector<int>(7, -1));
-
-    int ans = solve(3, 7, dp);
+    int ans = solve(3, 2);
     cout << ans;
 
     return 0;
