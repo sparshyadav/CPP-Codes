7a782c1d96375c7a1520347632c98798939475c2
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-18 11:37:13 +0530

    Finding All Unique Paths Using Tabulation

--- Dynamic Programming/Count_All_Unique_Paths.cpp
+++ Dynamic Programming/Count_All_Unique_Paths.cpp
@@ -29,37 +29,83 @@
 // }
 
 // Memoization
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int solve(int i, int j, vector<vector<int>> &dp)
+// {
+//     if (i == 0 && j == 0)
+//     {
+//         return 1;
+//     }
+
+//     if (i < 0 || j < 0)
+//     {
+//         return 0;
+//     }
+
+//     if (dp[i][j] != -1)
+//     {
+//         return dp[i][j];
+//     }
+
+//     int up = solve(i - 1, j, dp);
+//     int left = solve(i, j - 1, dp);
+
+//     return dp[i][j] = up + left;
+// }
+
+// int main()
+// {
+//     vector<vector<int>> dp(3, vector<int>(2, -1));
+
+//     int ans = solve(2, 1, dp);
+//     cout << ans;
+
+//     return 0;
+// }
+
+// Tabulation
 #include <bits/stdc++.h>
 using namespace std;
 
 int solve(int i, int j, vector<vector<int>> &dp)
 {
-    if (i == 0 && j == 0)
+    for (int row = 0; row < i; row++)
     {
-        return 1;
+        for (int col = 0; col < j; col++)
+        {
+            if (row == 0 && col == 0)
+            {
+                dp[0][0] = 1;
+            }
+            else
+            {
+                int up = 0;
+                if (row > 0)
+                {
+                    up = dp[row - 1][col];
+                }
+
+                int left = 0;
+                if (col > 0)
+                {
+                    left = dp[row][col - 1];
+                }
+
+                dp[row][col] = up + left;
+            }
+        }
     }
 
-    if (i < 0 || j < 0)
-    {
-        return 0;
-    }
-
-    if (dp[i][j] != -1)
-    {
-        return dp[i][j];
-    }
-
-    int up = solve(i - 1, j, dp);
-    int left = solve(i, j - 1, dp);
-
-    return dp[i][j] = up + left;
+    return dp[i - 1][j - 1];
 }
 
 int main()
 {
-    vector<vector<int>> dp(3, vector<int>(2, -1));
+    vector<vector<int>> dp(3, vector<int>(7, -1));
 
-    int ans = solve(2, 1, dp);
+    int ans = solve(3, 7, dp);
     cout << ans;
 
     return 0;
