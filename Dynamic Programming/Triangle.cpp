dcd030b98dcd9fca2b2eaf0e3afe2f8c1464fef2
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-19 09:57:44 +0530

    Finding the Minimal Path Cost in a Triangle Using Tabluation with Space Optimization

--- Dynamic Programming/Triangle.cpp
+++ Dynamic Programming/Triangle.cpp
@@ -59,28 +59,66 @@
 //     return 0;
 // }
 
-// Tabulation
+// // Tabulation
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int solve(int i, int j, int m, int n, vector<vector<int>> arr)
+// {
+//     vector<vector<int>> dp(m, vector<int>(n));
+
+//     for (int j = 0; j < n; j++)
+//     {
+//         dp[m - 1][j] = arr[m - 1][j];
+//     }
+
+//     for (int i = m - 2; i >= 0; i--)
+//     {
+//         for (int j = 0; j <= i; j++)
+//         {
+//             dp[i][j] = arr[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
+//         }
+//     }
+
+//     return dp[0][0];
+// }
+
+// int main()
+// {
+//     vector<vector<int>> arr = {{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}}; // ans - 11 -> 2, 3, 5, 1
+
+//     int ans = solve(0, 0, 4, 4, arr);
+//     cout << ans;
+
+//     return 0;
+// }
+
+// Tabulation with Space Optimization
 #include <bits/stdc++.h>
 using namespace std;
 
 int solve(int i, int j, int m, int n, vector<vector<int>> arr)
 {
-    vector<vector<int>> dp(m, vector<int>(n));
+    vector<int> nextRow(n);
 
     for (int j = 0; j < n; j++)
     {
-        dp[m - 1][j] = arr[m - 1][j];
+        nextRow[j] = arr[m - 1][j];
     }
 
     for (int i = m - 2; i >= 0; i--)
     {
+        vector<int> currRow(n);
+
         for (int j = 0; j <= i; j++)
         {
-            dp[i][j] = arr[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
+            currRow[j] = arr[i][j] + min(nextRow[j], nextRow[j + 1]);
         }
+
+        nextRow = currRow;
     }
 
-    return dp[0][0];
+    return nextRow[0];
 }
 
 int main()
