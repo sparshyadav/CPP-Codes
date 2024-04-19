6d411fb59e016ae06609b437c8bd0cef28372cbe
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-19 11:23:09 +0530

    Finding the Minimum Falling Path Sum Using Tabulation with Space Optimization

--- Dynamic Programming/Minimum_Falling_Path_Sum.cpp
+++ Dynamic Programming/Minimum_Falling_Path_Sum.cpp
@@ -85,45 +85,103 @@
 // }
 
 // Tabulation
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int solve(int m, int n, vector<vector<int>> arr)
+// {
+//     vector<vector<int>> dp(m, vector<int>(n));
+
+//     for (int j = 0; j < n; j++)
+//     {
+//         dp[m - 1][j] = arr[m - 1][j];
+//     }
+
+//     for (int i = m - 2; i >= 0; i--)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             int downLeft = INT_MAX;
+//             if (j > 0 && i < m - 1)
+//             {
+//                 downLeft = dp[i + 1][j - 1];
+//             }
+
+//             int downRight = INT_MAX;
+//             if (j < n - 1 && i < m - 1)
+//             {
+//                 downRight = dp[i + 1][j + 1];
+//             }
+
+//             int down = dp[i + 1][j];
+
+//             int ans = min(down, min(downLeft, downRight)) + arr[i][j];
+//             dp[i][j] = ans;
+//         }
+//     }
+
+//     int mini = INT_MAX;
+//     for (int i = 0; i < n; i++)
+//     {
+//         mini = min(dp[0][i], mini);
+//     }
+
+//     return mini;
+// }
+
+// int main()
+// {
+//     vector<vector<int>> arr = {{5, 7, 12}, {6, 5, 4}, {7, 8, 9}};
+
+//     int ans = solve(3, 3, arr);
+//     cout << ans;
+
+//     return 0;
+// }
+
+// Tabulation with Space Optimization
 #include <bits/stdc++.h>
 using namespace std;
 
 int solve(int m, int n, vector<vector<int>> arr)
 {
-    vector<vector<int>> dp(m, vector<int>(n));
+    vector<int> nextRow(n);
 
     for (int j = 0; j < n; j++)
     {
-        dp[m - 1][j] = arr[m - 1][j];
+        nextRow[j] = arr[m - 1][j];
     }
 
     for (int i = m - 2; i >= 0; i--)
     {
+        vector<int> currRow(n);
         for (int j = 0; j < n; j++)
         {
             int downLeft = INT_MAX;
             if (j > 0 && i < m - 1)
             {
-                downLeft = dp[i + 1][j - 1];
+                downLeft = nextRow[j - 1];
             }
 
             int downRight = INT_MAX;
             if (j < n - 1 && i < m - 1)
             {
-                downRight = dp[i + 1][j + 1];
+                downRight = nextRow[j + 1];
             }
 
-            int down = dp[i + 1][j];
+            int down = nextRow[j];
 
             int ans = min(down, min(downLeft, downRight)) + arr[i][j];
-            dp[i][j] = ans;
+            currRow[j] = ans;
         }
+
+        nextRow = currRow;
     }
 
     int mini = INT_MAX;
     for (int i = 0; i < n; i++)
     {
-        mini = min(dp[0][i], mini);
+        mini = min(nextRow[i], mini);
     }
 
     return mini;
@@ -131,7 +189,7 @@ int solve(int m, int n, vector<vector<int>> arr)
 
 int main()
 {
-    vector<vector<int>> arr = {{5, 7, 12}, {6, 5, 4}, {7, 8, 9}};
+    vector<vector<int>> arr = {{2, 1, 3}, {6, 5, 4}, {7, 8, 9}};
 
     int ans = solve(3, 3, arr);
     cout << ans;
