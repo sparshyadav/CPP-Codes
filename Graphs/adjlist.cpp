025e3239eb37f3f50242da665107217ef73cfa5f
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-17 15:59:04 +0530

    Adjcency List in Graphs

--- Graphs/adjlist.cpp
+++ Graphs/adjlist.cpp
@@ -1,6 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    int vertex, edges;
+    cin >> vertex, edges;
+
+    vector<int> AdjList[vertex];
+
+    int u, v;
+    for (int i = 0; i < edges; i++)
+    {
+        cin >> u >> v;
+        AdjList[u].push_back(v);
+        AdjList[v].push_back(v);
+    }
+
+    for (int i = 0; i < vertex; i++)
+    {
+        cout << i << " -> ";
+        for (int j = 0; j < AdjList[i].size(); i++)
+        {
+            cout << AdjList[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
 }
