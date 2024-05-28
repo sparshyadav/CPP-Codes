6be607b0ef9d92062555015a61cc3aea9b81ce5d
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-29 00:59:26 +0530

    Using Getter and Setter

--- OOPS/Student.cpp
+++ OOPS/Student.cpp
@@ -3,23 +3,36 @@ using namespace std;
 
 class Student
 {
-public:
+private:
     string name;
     int age;
     int rollNumber;
     string grade;
+
+public:
+    void setStudent(string n, int a, int r, string g)
+    {
+        name = n;
+        age = a;
+        rollNumber = r;
+        grade = g;
+    }
+
+    void getStudent()
+    {
+        cout << "Name - " << name << endl;
+        cout << "Age - " << age << endl;
+        cout << "RollNumber - " << rollNumber << endl;
+        cout << "Grade - " << grade << endl;
+    }
 };
 
 int main()
 {
 
     Student s1;
-    s1.name = "Sparsh";
-    s1.age = 22;
-    s1.rollNumber = 1383;
-    s1.grade = "A+";
-
-    cout << s1.name;
+    s1.setStudent("Sparsh", 22, 1383, "A+");
+    s1.getStudent();
 
     return 0;
 }
