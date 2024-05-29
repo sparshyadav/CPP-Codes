d6d887adf9693d1fbfa025caad6ccc2d8bd6afaf
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-29 11:28:57 +0530

    Created Constructor for Invoking an Object

---
+++ OOPS/Customer.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Customer
+{
+    string name;
+    int accountNumber;
+    int balance;
+
+public:
+    Customer(string name, int accountNumber, int balance)
+    {
+        this->name = name;
+        this->accountNumber = accountNumber;
+        this->balance = balance;
+    }
+
+    void Display()
+    {
+        cout << "Name - " << name << endl;
+        cout << "Account Number - " << accountNumber << endl;
+        cout << "Balance - " << balance << endl;
+    }
+};
+
+int main()
+{
+    Customer a1("Sparsh", 43827372, 10000000);
+    a1.Display();
+}
