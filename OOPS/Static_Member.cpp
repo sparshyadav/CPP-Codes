98c3e38300bdf14d6b069f838d424e5e25a90c40
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-06-07 07:23:49 +0530

    Accessing Static Data Member Using Class

--- OOPS/Static_Member.cpp
+++ OOPS/Static_Member.cpp
@@ -6,9 +6,9 @@ class Customer
     string name;
     int accountNumber;
     int balance;
-    static int totalCustomer;
 
 public:
+    static int totalCustomer;
     Customer(string name, int accountNumber, int balance)
     {
         this->name = name;
@@ -25,18 +25,26 @@ public:
         cout << "Total Customers - " << totalCustomer << endl;
         cout << endl;
     }
+
+    void displayTotalCustomers()
+    {
+        cout << "Total Customers - " << totalCustomer << endl;
+    }
 };
 
 int Customer ::totalCustomer = 0;
 
 int main()
 {
+    Customer ::totalCustomer = 5;
 
     Customer c1("Sparsh", 1383, 5000);
-    Customer c2("Shreya", 1421, 10000);
-
     c1.display();
+
+    Customer c2("Shreya", 1421, 10000);
     c2.display();
+
+    c1.displayTotalCustomers();
 
     return 0;
 }
