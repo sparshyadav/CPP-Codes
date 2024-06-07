05c4b393816bcded7defbaa50b874a25eb84ec82
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-06-07 07:33:21 +0530

    Using Static Functions

--- OOPS/Static_Member.cpp
+++ OOPS/Static_Member.cpp
@@ -6,15 +6,41 @@ class Customer
     string name;
     int accountNumber;
     int balance;
+    static int totalBalance;
+    static int totalCustomer;
 
 public:
-    static int totalCustomer;
     Customer(string name, int accountNumber, int balance)
     {
         this->name = name;
         this->accountNumber = accountNumber;
         this->balance = balance;
         totalCustomer++;
+        totalBalance += balance;
+    }
+
+    static void accessStatic()
+    {
+        cout << "Total Customers - " << totalCustomer << endl;
+        cout << "Total Balance - " << totalBalance << endl;
+    }
+
+    void deposit(int amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+            totalBalance += amount;
+        }
+    }
+
+    void withdraw(int amount)
+    {
+        if (amount > 0)
+        {
+            balance -= amount;
+            totalBalance -= amount;
+        }
     }
 
     void display()
@@ -33,16 +59,25 @@ public:
 };
 
 int Customer ::totalCustomer = 0;
+int Customer ::totalBalance = 0;
 
 int main()
 {
-    Customer ::totalCustomer = 5;
+    // Customer ::accessStatic();
 
     Customer c1("Sparsh", 1383, 5000);
-    c1.display();
+    // c1.display();
 
     Customer c2("Shreya", 1421, 10000);
-    c2.display();
+    // c2.display();
+
+    c1.withdraw(2000);
+
+    Customer ::accessStatic();
+
+    c2.deposit(3000);
+
+    Customer::accessStatic();
 
     c1.displayTotalCustomers();
 
