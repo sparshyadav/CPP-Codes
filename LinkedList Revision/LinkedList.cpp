4a1415a8c16f4fd6c486a9d6b513c98d1236e393
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-08-30 04:32:10 +0530

    Implemented InsertAtHead and Display Function

--- LinkedList Revision/LinkedList.cpp
+++ LinkedList Revision/LinkedList.cpp
@@ -12,13 +12,31 @@ public:
         data = val;
         next = NULL;
     }
+
+    void insertAtHead(Node *head, int val)
+    {
+        Node *newNode = new Node(val);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void display(Node *head)
+    {
+        Node *temp = head;
+
+        while (temp)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
 };
 
 int main()
 {
     Node *head = new Node(4);
-    cout << head->data<<endl;
-    cout << head->next<<endl;
+    cout << head->data << endl;
+    cout << head->next << endl;
 
     return 0;
 }
